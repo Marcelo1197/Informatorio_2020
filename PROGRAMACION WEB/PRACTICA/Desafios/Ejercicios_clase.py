b78b570614f19621299452902d0a9a48c62a61ef
Marcelo1197 <sanchez.marcelo2010@gmail.com>
2020-06-23 01:51:13 -0300

    Desafios Condicionales, repetitivas y E. Control terminados. Falta 1 desafio de Listas Tuplas Directorio

---
+++ PROGRAMACION WEB/PRACTICA/Desafios/Ejercicios_clase.py
@@ -0,0 +1,22 @@
+# EJERCICIO 2
+""""
+lista = []
+
+for i in range(0, 3):
+	asignatura = input("Ingrese el nombre de la asignatura: ")
+	lista.append(asignatura)
+
+for elemento in lista:
+	print("Yo estudio ", elemento)
+"""
+
+lista = []
+
+for i in range(0, 3):
+	asignatura = input("Ingrese el nombre de la asignatura:\t")
+	nota = int(input("Ingrese su nota:\t"))
+	asignatura_nota.append(asignatura, nota)
+	lista.append()
+
+	for i in range(len(lista)):
+		print("En ", lista[i][i][i+1])
